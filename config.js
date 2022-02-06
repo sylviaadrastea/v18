7b9886e04fcbedba5bbd35c6b084a5a96704171b
Sylvia <98797671+sylviaadrastea@users.noreply.github.com>
2022-02-06 16:38:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,11 +15,11 @@
 gc1 = ''
 gc2 = ''
 gc3 = ''
-global.linkGC = ['https://chat.whatsapp.com/HAZ6yFgCafUAeDbNH33IrL', ''] // ganti jadi group lu
-global.owner = ['6288279268363'] // Put your number here //owner eval
-global.kontak = ['6288279268363','0'] //Ketika ada yang ngetik #owner
-global.mods = ['6288279268363'] // Want some help?
-global.prems = ['6288279268363'] // Premium user has unlimited limit
+global.linkGC = ['', ''] // ganti jadi group lu
+global.owner = ['62857980707940'] // Put your number here //owner eval
+global.kontak = ['62857980707940','0'] //Ketika ada yang ngetik #owner
+global.mods = ['62857980707940'] // Want some help?
+global.prems = ['6285798070794'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
@@ -55,22 +55,22 @@ bearkey = 'buy'
 
 //xteam        MIMINETBOT
 namaig = 'https://instagram.com/drak_ipul123'
-namagithub = 'GAK ADA :)'
+namagithub = 'https://github.com/sylviaadrastea'
 kasihcaption = `Nih kak`
-namakontak1 = 'King Of Bear'
-namakontak2 = 'King Of Bear'
+namakontak1 = 'sylvia adrastea'
+namakontak2 = 'sylviaadrastea'
 
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
-global.packname = 'By King Of Bear' // ganti aja
-global.author = 'King Of Bear' // ganti aja
+global.packname = 'sylvia' // ganti aja
+global.author = 'VIA BOT' // ganti aja
 
 //yyy
-bc = 'King Of Bear' //King Of Bear Broadcast
-footer = '\n©King Of Bear'
-namabot = 'YUKI BOT'
-namalu = 'King Of Bear'
+bc = 'VIA BOT' //King Of Bear Broadcast
+footer = '\n©sylvia adrastea'
+namabot = 'VIA BOT'
+namalu = 'sylvia adrastea'
 
 
 // 
